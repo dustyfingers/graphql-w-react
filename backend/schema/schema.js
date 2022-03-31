cf1fd059e24ff06700e568f69f65a7d93d53ebff
lou <dustyfingers01257@gmail.com>
2022-03-30 23:40:17 -0600

    connected users and companies

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -2,24 +2,34 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = require('graphql');
 const axios = require('axios');
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    // because of the way closures work in js, this works. the field object is no longer in this scope: it is in the arrow functions scope
+    // which doesnt get executed until everything in this file is defined correctly
+    // HAS TO BE EXACTLY LIKE THIS TO NOT GET A CIRCULAR REFERENCE ERROR fields: () => ({ id: blah blah blah })
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentVal, args) {
+                return axios.get(`http://localhost:3000/users`).then(res => res.data.filter(user => user.companyId === parentVal.id))
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     // name of an individual record - REQUIRED
     name: 'User',
     // fields object tells graphql about all the properties a User has
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         age: { type: GraphQLInt },
         firstName: { type: GraphQLString },
@@ -30,10 +40,10 @@ const UserType = new GraphQLObjectType({
                 return axios.get(`http://localhost:3000/companies/${parentVal.companyId}`).then(res => res.data);
             }
         }
-    }
+    })
 });
 
-// RootQuery tells graphql where to hook into our data - eg at user #23, then
+// RootQuery tells graphql where to hook into the graph representation of our data - eg at user 123, or company 2
 // look from there
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -46,6 +56,14 @@ const RootQuery = new GraphQLObjectType({
             // the resolve function fetches the data and resolves to a value
             resolve(parentVal, args) {
                 return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data);
+            }
+        },
+
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentVal, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`).then(res => res.data);
             }
         }
     }
